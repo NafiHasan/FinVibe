cfdc119f9f48b6a5d2b2e4bfb8c701ed1a546117
Hasan Nur Towha <nurtowha@gmail.com>
2024-06-15 07:25:43 -0700

    huda

--- frontend/react-app/src/scripts/MakePost.jsx
+++ frontend/react-app/src/scripts/MakePost.jsx
@@ -2,6 +2,7 @@
 import NavigationBar from './NavigationBar'
 import {useLocation} from 'react-router-dom';
 import '../styles/MakePost.css'
+import { useState } from 'react';
 
 function MakePost(){
     const location = useLocation();
@@ -21,6 +22,28 @@ function MakePostBody(props) {
     function createPost(){
 
     }
+
+    const [selectedImages, setSelectedImages] = useState([]);
+
+    const onSelectFile = (event) => {
+      const selectedFiles = event.target.files;
+      const selectedFilesArray = Array.from(selectedFiles);
+
+      const imagesArray = selectedFilesArray.map((file) => {
+        return URL.createObjectURL(file);
+      });
+
+      setSelectedImages((previousImages) => previousImages.concat(imagesArray));
+
+      // FOR BUG IN CHROME
+      event.target.value = "";
+    };
+
+    function deleteHandler(image) {
+      setSelectedImages(selectedImages.filter((e) => e !== image));
+      URL.revokeObjectURL(image);
+    }
+
     return (
         <div className='makePostBody'>
             <div className='left'>
@@ -29,8 +52,20 @@ function MakePostBody(props) {
 
 
             <div className='right'>
+                <div>
+                            <input
+                      type="file"
+                      name="images"
+                      onChange={onSelectFile}
+                      multiple
+                      accept="image/png , image/jpeg, image/webp"
+                    />
+              
+                </div>
                 <button onClick={createPost}>Post</button>
             </div>
+
+
         </div>
     )
 }
