12b1ec62ef141ed8c00b3d61a327605c4d659b5c
NafiHasan <nafihasan141@gmail.com>
2024-06-18 12:44:26 +0600

    graph integrated with backend

--- frontend/react-app/src/scripts/Crypto.jsx
+++ frontend/react-app/src/scripts/Crypto.jsx
@@ -1,49 +1,48 @@
-import '../styles/Crypto.css'
-import NavigationBar from './NavigationBar'
-import CryptoCard from './CryptoCard'
-import {useLocation} from 'react-router-dom';
-import TrendingTab from './TrendingTab'
+import "../styles/Crypto.css";
+import NavigationBar from "./NavigationBar";
+import CryptoCard from "./CryptoCard";
+import { useLocation } from "react-router-dom";
+import TrendingTab from "./TrendingTab";
 
-function Crypto(){
-    const location = useLocation();
+function Crypto() {
+  const location = useLocation();
 
-    let username = location.state.username
+  let username = location.state.username;
 
-    return(
-        <div className='cryptoMainBody'>
-            <NavigationBar username = {username}/>
-            <CryptoBody username = {username}/>
-        </div>
-    )
+  return (
+    <div className="cryptoMainBody">
+      <NavigationBar username={username} />
+      <CryptoBody username={username} />
+    </div>
+  );
 }
 
-function CryptoBody(props){
-    return(
-        <div className='cryptoBody'>
-            <CryptoLeftHalf {...props}/>
-            <CryptoRightHalf {...props}/>
-        </div>
-    )
+function CryptoBody(props) {
+  return (
+    <div className="cryptoBody">
+      <CryptoLeftHalf {...props} />
+      <CryptoRightHalf {...props} />
+    </div>
+  );
 }
 
-function CryptoLeftHalf(props){
-    return(
-        <div className='cryptoLeftColumn'>
-            <TrendingTab/>
-        </div>
-    )
+function CryptoLeftHalf(props) {
+  return (
+    <div className="cryptoLeftColumn">
+      <TrendingTab />
+    </div>
+  );
 }
 
-function CryptoRightHalf(props){
-    return(
-        <div className='cryptoRightColumn'>
+function CryptoRightHalf(props) {
+  return (
+    <div className="cryptoRightColumn">
+      <CryptoCard {...props} />
+      {/* <CryptoCard {...props}/>
             <CryptoCard {...props}/>
-            <CryptoCard {...props}/>
-            <CryptoCard {...props}/>
-            <CryptoCard {...props}/>
-        </div>
-    )
+            <CryptoCard {...props}/> */}
+    </div>
+  );
 }
 
-export default Crypto
-
+export default Crypto;
